a0f92f52637c8299e088a6d3f21f73c4d22db47d
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-25 21:42:36 -0400

    APlicacion casi terminada, solo falta probar en pantallas grandes

--- app/src/main/java/com/pablo384/mymail/Activities/MainActivity.kt
+++ app/src/main/java/com/pablo384/mymail/Activities/MainActivity.kt
@@ -5,23 +5,36 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.pablo384.mymail.Fragments.DetailFragment
 import com.pablo384.mymail.Fragments.HomeFragment
 import com.pablo384.mymail.Models.Mail
 import com.pablo384.mymail.R
 
 class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionListenerHomeFragment {
-    override fun onFragmentInteractionHomeFragmentData(mail: Mail) {
 
-        startActivity(Intent(this,DetailActivity::class.java)
-                .putExtra(
-                        "mail",
-                        Mail(mail.subject,mail.from,mail.body)
-                ))
 
-    }
-
+    private var ismultiPanel = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setMultiPanel()
+    }
+
+    override fun onFragmentInteractionHomeFragmentData(mail: Mail) {
+        if (ismultiPanel) {
+            val frDetail = supportFragmentManager.findFragmentById(R.id.fragmentDetail) as DetailFragment
+            frDetail.renderText(Mail(mail.subject, mail.from, mail.body))
+
+        } else {
+            startActivity(Intent(this, DetailActivity::class.java)
+                    .putExtra(
+                            "mail",
+                            Mail(mail.subject, mail.from, mail.body)
+                    ))
+        }
+    }
+
+    fun setMultiPanel() {
+        ismultiPanel = (supportFragmentManager.findFragmentById(R.id.fragmentDetail) != null)
     }
 }
